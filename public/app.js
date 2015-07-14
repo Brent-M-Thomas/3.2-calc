98adb2c514d51ee0a99e2fe5a75d0a76ab378cf7
Brent Thomas <brentmt24@gmail.com>
2015-07-14 15:57:34 -0500

    modified in class

---
+++ public/app.js
@@ -0,0 +1,26 @@
+var modeEl = document.getElementById("mode");
+var valueEl = document.getElementById("value");
+var valueEl = document.getElementById("clear");
+
+var numButtons = document.getElementsByClassName("number-button");
+var modeButtons = document.getElementsByClassName("mode");
+
+clearEl.onclick = function () {
+    valueEl.innerHTML = 0 ;
+}
+var buttonSubscribe = function() {
+    var currentNum = parseInt(this.innerHTML);
+    var tmp;
+
+    if (modeEL.innerHTML == "+"){
+    tmp = currentNum + parseInt(valueEl.innerHTML);
+} else {
+    tmp = parseInt(valueEl.innerHTML) - currentNum;
+};
+var modeClick = function (){
+    modeEl.innerHTML = this.innerHTML;
+}
+for (var i = numButtons.length - 1; i >= 0; i--) {
+    numButtons[i].onclick = buttonSubscribe;
+};
+
